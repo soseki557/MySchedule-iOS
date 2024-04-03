b7c5fa12faa0094af9243498e5939c2eac090be4
soseki557 <soseki557@gmail.com>
2024-04-03 23:07:14 +0900

    ItemをTaskに置き換え

--- MySchedule/ContentView.swift
+++ MySchedule/ContentView.swift
@@ -10,16 +10,16 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+    @Query private var tasks: [Task]
 
     var body: some View {
         NavigationSplitView {
             List {
-                ForEach(items) { item in
+                ForEach(tasks) { task in
                     NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
+                        Text("Item at \(task.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
                     } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
+                        Text(task.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
                     }
                 }
                 .onDelete(perform: deleteItems)
@@ -41,7 +41,7 @@ struct ContentView: View {
 
     private func addItem() {
         withAnimation {
-            let newItem = Item(timestamp: Date())
+            let newItem = Task(timestamp: Date())
             modelContext.insert(newItem)
         }
     }
@@ -49,7 +49,7 @@ struct ContentView: View {
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
             for index in offsets {
-                modelContext.delete(items[index])
+                modelContext.delete(tasks[index])
             }
         }
     }
@@ -57,5 +57,5 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+        .modelContainer(for: Task.self, inMemory: true)
 }
--- MySchedule/Task.swift
+++ MySchedule/Task.swift
@@ -1,5 +1,5 @@
 //
-//  Item.swift
+//  Task.swift
 //  MySchedule
 //
 //  Created by YOSHiTAK on 2024/03/30.
@@ -9,9 +9,14 @@ import Foundation
 import SwiftData
 
 @Model
-final class Item {
+final class Task {
+    // タイトル
+    // メモ
+    // 開始日時
+    // 終了日時
+    // 終日フラグ
+    // 通知
     var timestamp: Date
-    
     init(timestamp: Date) {
         self.timestamp = timestamp
     }
